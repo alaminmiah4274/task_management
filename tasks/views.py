d88a73a65b7df8604c1178d8122529368a33992b
AlAminMiah4274 <alaminmiah4274@gmail.com>
2025-01-17 12:46:21 +0600

    add templates and static files

--- tasks/views.py
+++ tasks/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-	# work with database
-	# transform data
-	# data pass
-	# http response / json response return
-	return HttpResponse("Welcome to the task management system")
+
+def manager_dashboard(request):
+	return render(request, "dashboard/manager_dashboard.html")
 
 
-def contact(request):
-	return HttpResponse("<h1 style='color: red'>this is contact page response ...<h1>")
+def user_dashboard(request):
+	return render(request, "dashboard/user_dashboard.html")
 
 
-def show_task(request):
-	return HttpResponse("this is task page")
+def test_static(request):
+
+	names = ["miah", "howlader", "billah", "shafim", "sheikh"]
+	
+	count = 0
+	for name in names:
+		count += 1
+
+	context = {
+		"names": names,
+		"ages": [23, 25, 27, 23, 25],
+		"count": count,
+	}
+
+	return render(request, "test.html", context)
