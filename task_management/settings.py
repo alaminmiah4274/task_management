60fcc17193a80e51a0519325b4a47458b2fc2968
AlAminMiah4274 <alaminmiah4274@gmail.com>
2025-01-19 18:44:32 +0600

    database setup, model, field, relationship, reverse relation, related name and django shell

--- task_management/settings.py
+++ task_management/settings.py
@@ -75,10 +75,25 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# SQLite 3 database:
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# PostgreSQL database:
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': '...',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
